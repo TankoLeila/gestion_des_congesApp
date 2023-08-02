dc6c793210274eec19d785e5ce86a5ea4f838016
TankoLeila <tanko.leila123@gmail.com>
2023-08-02 12:40:42 +0100

    Improve the login error message

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -8,21 +8,35 @@
     </section>
 
     <section
-      class="flex flex-col gap-y-10 md:gap-y-2 lg:gap-y-2 md:items-center lg:items-center shadow-lg border h-[700px] md:h-[500px] lg:h-[500px] px-5 md:bg-white lg:bg-white md:w-[400px] md:p-5 lg:w-[400px] lg:p-5 md:flex md:rounded-2xl">
+      class="flex flex-col gap-y-10 md:gap-y-2 lg:gap-y-2 md:items-center lg:items-center shadow-lg border h-[700px] md:h-[600px] lg:h-[600px] px-5 md:bg-white lg:bg-white md:w-[400px] md:p-5 lg:w-[400px] lg:p-5 md:flex md:rounded-2xl"
+    >
       <div class="flex flex-col gap-y-2 p-5">
         <span class="text-2xl font-bold">Login</span>
         <span class="">Welcome and Identify you here!!</span>
       </div>
       <div class="space-y-12">
-        <div class="bg-red-700 text-white p-4 rounded-lg" v-if="errorMessage">
+        <div class="text-red-500 p-4 rounded-lg" v-if="errorMessage">
           {{ errorMessage }}
         </div>
-        <FormInput :errors="v$.email.$errors" v-model="email" label="Email" placeholder="Enter your email" type="email" />
-        <FormInput v-model="password" type="password" placeholder="Enter your Password" label="Password"
-          :errors="v$.password.$errors" />
+        <FormInput
+          :errors="v$.email.$errors"
+          v-model="email"
+          label="Email"
+          placeholder="Enter your email"
+          type="email"
+        />
+        <FormInput
+          v-model="password"
+          type="password"
+          placeholder="Enter your Password"
+          label="Password"
+          :errors="v$.password.$errors"
+        />
         <div class="border-blue-200">
-          <button @click="loginToApp"
-            class="bg-blue-400 text-white font-bold rounded-md border-2 py-3 px-2 w-full shadow-lg shadow-blue-400">
+          <button
+            @click="loginToApp"
+            class="bg-blue-400 text-white font-bold rounded-md border-2 py-3 px-2 w-full shadow-lg shadow-blue-400"
+          >
             LOGIN
           </button>
         </div>
@@ -68,9 +82,9 @@ export default {
     async loginToApp() {
       const isFormValid = await this.v$.$validate()
       if (isFormValid) {
-        if (this.email === "tanko.leila123@gmail.com" && this.password === "leilaTanko") {
-          this.$router.push("/admin/employees")
-          return;
+        if (this.email === 'tanko.leila123@gmail.com' && this.password === 'leilatanko') {
+          this.$router.push('/admin/employees')
+          return
         }
         const id = await this.store.loginEmployee({
           email: this.email,
@@ -86,16 +100,16 @@ export default {
   watch: {
     email(newEmail) {
       if (newEmail && this.store.error.email) {
-        Object.assign(this.store.error, { email: "", password: "" })
-        Object.assign(this.vuelidateExternalResults, { email: "", password: "" })
+        Object.assign(this.store.error, { email: '', password: '' })
+        Object.assign(this.vuelidateExternalResults, { email: '', password: '' })
       }
     },
     password(newPassword) {
       if (newPassword && this.store.error.password) {
-        Object.assign(this.store.error, { email: "", password: "" })
-        Object.assign(this.vuelidateExternalResults, { email: "", password: "" })
+        Object.assign(this.store.error, { email: '', password: '' })
+        Object.assign(this.vuelidateExternalResults, { email: '', password: '' })
       }
-    },
+    }
   },
   computed: {
     errorMessage() {
